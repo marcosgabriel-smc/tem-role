5069c90ac8f0b753b2c0ca04a1750ff66517a734
Augusto Lopes <meleu.dev@gmail.com>
2023-06-06 12:23:59 -0300

    silent some tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,4 +10,7 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  # include Devise::Test::IntegrationHelpers
+  include Warden::Test::Helpers
+  Warden.test_mode!
 end
