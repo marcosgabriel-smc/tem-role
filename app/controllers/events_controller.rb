109072fedbbda756101d4157157d41126b8e33da
Augusto Lopes <meleu.dev@gmail.com>
2023-06-07 18:49:20 -0300

    WIP

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -20,6 +20,7 @@ class EventsController < ApplicationController
   # POST /events or /events.json
   def create
     @event = Event.new(event_params)
+    raise
 
     if @event.save
       redirect_to event_url(@event), notice: "Event was successfully created."
@@ -62,6 +63,14 @@ class EventsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def event_params
-    params.require(:event).permit(:name, :description, :start_time, :start_end)
+    params.require(:event).permit(
+      :name,
+      :description,
+      :start_time,
+      :start_end,
+      :collective_id,
+      :genre_ids,
+      :city
+    )
   end
 end
