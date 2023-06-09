a3b38941ee2a3aadb4284ab1cba34021e14c13c6
Marcos Costa <marcosgabriel.smc@gmail.com>
2023-06-09 14:54:17 -0300

    WIP - Show Event

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_07_200705) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_09_174129) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -102,6 +102,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_07_200705) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.text "description"
+    t.string "photo1"
+    t.string "photo2"
+    t.string "photo3"
     t.index ["collective_id"], name: "index_events_on_collective_id"
   end
 
